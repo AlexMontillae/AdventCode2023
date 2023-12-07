00f2e5138ec1918ca2e427232121824c7563cd76
Alex Montilla <alex.montilla.e16@gmail.com>
2023-12-07 15:35:10 -0400

    Agrego la nueva función num_in_value para completar la parte 2 del dia1

--- day1.py
+++ day1.py
@@ -2,7 +2,41 @@ def calibration_value(value: str) -> int:
     """
     Receives a string and returns the first and the last digit of it
     """
-    numbers = list(filter(lambda x: x in '0123456789' , value))
+    def num_in_value(value: str) -> str:
+        """
+        receives a string and returns another string of digits and if there is any number spelled it also returns it as a digit
+        F.E:'one' == '1'
+            'two' == '2'
+            ....
+            'nine' == '9'
+            """
+        str_of_numbers = ''
+        for i, elem in enumerate(value):
+        
+            if elem in '0123456789':
+                str_of_numbers += elem
+            elif value[i:i+3] == 'one':
+                str_of_numbers += '1'
+            elif value[i:i+3] == 'two':
+                str_of_numbers += '2'
+            elif value[i:i+5] == 'three':
+                str_of_numbers += '3'
+            elif value[i:i+4] == 'four':
+                str_of_numbers += '4'
+            elif value[i:i+4] == 'five':
+                str_of_numbers += '5'
+            elif value[i:i+3] == 'six':
+                str_of_numbers += '6'
+            elif value[i:i+5] == 'seven':
+                str_of_numbers += '7'
+            elif value[i:i+5] == 'eight':
+                str_of_numbers += '8'
+            elif value[i:i+4] == 'nine':
+                str_of_numbers += '9'
+
+        return str_of_numbers
+
+    numbers = num_in_value(value)
     numbers = int(numbers[0] + numbers[-1])
 
     return numbers
