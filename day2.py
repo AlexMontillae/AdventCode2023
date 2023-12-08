956a8ec50d24417c1fb604e0eac43c74940a038c
Alex Montilla <alex.montilla.e16@gmail.com>
2023-12-08 01:32:43 -0400

    adapto el codigo con respecto a los requisitos de la parte 2 del dia2

--- day2.py
+++ day2.py
@@ -2,28 +2,20 @@
 
 def main(text: str) -> int:
     """
-    Receives a text file and returns the sum of the ID's of the games that passed the test
+    Receives a text file and returns the sum of the products of the minimum sets of cubes per game
     """
     total = 0
 
     new_list = text.split('\n')
-    new_list = list(map(lambda x: game_info(x), new_list))
-    for i, elem in enumerate(new_list):
-        if elem == True:
-            total += i +1
+    new_list = list(map(lambda x: game_info(x), new_list))    
+    total = sum(new_list)
 
     return total
 
-def game_info(game: str) -> bool:
+def game_info(game: str) -> int:
     """
-    Receives a string of the game and return a bool)
-    The bool value = True if the game is possible
-    The bool value = False if the game is not possible
-    A game is possible if the sum of all the number of times <= 12 red, 13 green and 14 blue
+    Receives a string of the game and return the product of the minimum sets of cubes
     """
-
-    # inicializamos nuestra variable
-    result = False
     # normalizamos nuestra cadena para solo obtener los valores despues del ':'
     new_list = ''
     for i,char in enumerate(game):
@@ -32,32 +24,26 @@ def game_info(game: str) -> bool:
             break
     new_list = new_list.split(';')
     
-    
+    green = 1
+    red = 1
+    blue = 1
 
     # iteramos por cada elemento de la nueva lista
-    # agregamos el valor de cada jugada a las variables de colores
+    # verificamos si el numero es mayor que la variable por color
+    # si es mayor sustituimos el valor por el numero actual
     for elem in new_list:
-
-        # una vez ya normalizada, creamos las variables green, red and blue para almacenar la canditad de veces que aparecen los cubos por jugada
-        green = 0
-        red = 0
-        blue = 0
         for play in elem.split(','):
             if 'green' in play:
-                green += extract_num(play)
+                if extract_num(play) > green:
+                    green = extract_num(play)
             if 'blue' in play:
-                blue += extract_num(play)
+                if extract_num(play) > blue:
+                    blue = extract_num(play)
             if 'red' in play:
-                red += extract_num(play)
+                if extract_num(play) > red:
+                    red = extract_num(play)
 
-        # ahora vemos si la cantidad de veces que salen los cubos es posible 
-        if red <= 12 and green <= 13 and blue <= 14:
-            result = True
-        else:
-            result = False
-            break
-    
-
+    result = green * blue * red
     return result
 
 def extract_num(string: str) -> int:
