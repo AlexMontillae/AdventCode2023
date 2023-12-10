5793fe49f1abcd507e2997b883b5c8a1a25f1304
Alex Montilla <alex.montilla.e16@gmail.com>
2023-12-09 21:04:24 -0400

    Se modifica la función adjacent_nums para completar la parte 2 del dia3

--- day3.py
+++ day3.py
@@ -8,7 +8,7 @@ def main(text: str) -> int:
         s = s.split('\n')
         for i,elem in enumerate(s):
             for j,char in enumerate(elem):
-                if char in '" #$%&()*+,-/:;<=>?@[\\]^_`{|}~':
+                if char == '*':
                     new += adjacent_nums(s,i,j)
     
     return new
@@ -16,7 +16,7 @@ def main(text: str) -> int:
 
 def adjacent_nums(list_of_str: list[str], i , j)-> int:
 
-    total = 0
+    total = []
 
     left = list_of_str[i][j-1]
     right = list_of_str[i][j+1]
@@ -28,37 +28,43 @@ def adjacent_nums(list_of_str: list[str], i , j)-> int:
     down_right = list_of_str[i+1][j+1]
 
     if left.isalnum():
-        total += right_or_left('left', list_of_str[i][j-3:j])
+        total.append(right_or_left('left', list_of_str[i][j-3:j]))
     if right.isalnum():
-        total += right_or_left('right', list_of_str[i][j+1:j+4])
+        total.append(right_or_left('right', list_of_str[i][j+1:j+4]))
     if up.isalnum() and up_left.isalnum() and not up_right.isalnum():
-        total += right_or_left('left', list_of_str[i-1][j-2:j+1])
+        total.append(right_or_left('left', list_of_str[i-1][j-2:j+1]))
     if up.isalnum() and up_right.isalnum() and not up_left.isalnum():
-        total += right_or_left('right', list_of_str[i-1][j:j+3])
+        total.append(right_or_left('right', list_of_str[i-1][j:j+3]))
     if up.isalnum() and up_right.isalnum() and up_left.isalnum():
-        total += right_or_left('middle', list_of_str[i-1][j-1:j+2])
+        total.append(right_or_left('middle', list_of_str[i-1][j-1:j+2]))
     if up_left.isalnum() and not up.isalnum():
-        total += right_or_left('left', list_of_str[i-1][j-3:j])
+        total.append(right_or_left('left', list_of_str[i-1][j-3:j]))
     if up_right.isalnum() and not up.isalnum():
-        total += right_or_left('right', list_of_str[i-1][j+1:j+4])
+        total.append(right_or_left('right', list_of_str[i-1][j+1:j+4]))
     if up.isalnum() and not up_right.isalnum() and not up_left.isalnum():
-        total += int(list_of_str[i-1][j])
+        total.append(int(list_of_str[i-1][j]))
     if down.isalnum() and down_left.isalnum() and not down_right.isalnum():
-        total += right_or_left('left',list_of_str[i+1][j-2:j+1])
+        total.append(right_or_left('left',list_of_str[i+1][j-2:j+1]))
     if down.isalnum() and down_right.isalnum() and not down_left.isalnum():
-        total += right_or_left('right', list_of_str[i+1][j:j+3])
+        total.append(right_or_left('right', list_of_str[i+1][j:j+3]))
     if down_left.isalnum() and not down.isalnum():
-        total += right_or_left('left', list_of_str[i+1][j-3:j])
+        total.append(right_or_left('left', list_of_str[i+1][j-3:j]))
     if down_right.isalnum() and not down.isalnum():
-        total += right_or_left('right', list_of_str[i+1][j+1:j+4])
+        total.append(right_or_left('right', list_of_str[i+1][j+1:j+4]))
     if down.isalnum() and down_right.isalnum() and down_left.isalnum():
-        total += right_or_left('middle', list_of_str[i+1][j-1:j+2])
+        total.append(right_or_left('middle', list_of_str[i+1][j-1:j+2]))
     if down.isalnum() and not down_right.isalnum() and not down_left.isalnum():
-        total += int(list_of_str[i+1][j])
+        total.append(int(list_of_str[i+1][j]))
+
+    if len(total) == 2:
+        from functools import reduce
+        total = reduce(lambda x,y: x*y, total)
+    else:
+        total = 0
 
     return total
 
-def right_or_left(decision: str, iterable:str) -> int:
+def right_or_left(decision: str, iterable:str) -> str:
 
     total = ''
     if decision == 'right':
